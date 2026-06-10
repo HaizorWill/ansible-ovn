d177fc951928a6cba64832a95da2d556a8d29255
HaizorWill <Thelines6@gmail.com>
2026-06-10 17:33:56 +0300

    Update: add single transaction for write/delete instead of separate calls
    Now module runs a transaction method that first calls preflight and
    fetches data from database to then compare it with the desired state,
    then it sends delete and insert in one single atomic transaction

--- library/ovsdb-externalids.py
+++ library/ovsdb-externalids.py
@@ -2,96 +2,79 @@
 
 from __future__ import (absolute_import, division, print_function)
 import os
-from re import split
-import time
 from typing import Any
 __metaclass__ = type
 
 from ansible.module_utils.basic import AnsibleModule
-from typing import NewType
 
-Exists = NewType("Exists", bool)
-EXISTS: Exists = Exists(True)
-NOT_EXISTS: Exists = Exists(False)
+type Changed = bool
+IS_CHANGED: Changed = True
+IS_NOT_CHANGED: Changed = False
 
-Changed = NewType("Changed", bool)
-IS_CHANGED: Changed = Changed(True)
-IS_NOT_CHANGED: Changed = Changed(False)
+type Err = str
 
 import traceback
 import ovs.stream
-import ovs.dirs
-import ovs.poller
 import ovs.jsonrpc
 
-def checkKey(conn: ovs.jsonrpc.Connection, key: str) -> tuple[Any, Exists, str | None]:
+def MakeTransaction(conn: ovs.jsonrpc.Connection, newState: dict[str, str], checkMode: bool) -> tuple[Changed, Any, Err | None]:
+    state, err = preflight(conn)
+    if err or state == None:
+        return IS_NOT_CHANGED, None, err
+    if not state.items():
+        return IS_NOT_CHANGED, None, f"Database preflight returned nothing: {state.items()}"
+    change = dict()
+    for k, v in newState.items():
+        if state.get(k) != v:
+           change[k] = v
+
+    if not change.items():
+        return IS_NOT_CHANGED, "Already up to date", None
+
+    if checkMode:
+        return IS_CHANGED, str(change), None
+
+    mutQuery = [
+            ["external_ids", "delete", ["set", list(change)]],
+            ["external_ids", "insert", ["map", list(change.items())]]
+    ]
+
+    request = ovs.jsonrpc.Message.create_request("transact", [
+        "Open_vSwitch",
+        {"op": "mutate", "table": "Open_vSwitch", "where": [], "mutations": mutQuery}
+        ])
+    err, resp = conn.transact_block(request)
+    if err:
+        return IS_NOT_CHANGED, None, os.strerror(err)
+    if resp is None:
+        return IS_NOT_CHANGED, None, 'OVSDB server did not reply'
+    if resp.type == ovs.jsonrpc.Message.T_ERROR or resp.error is not None:
+        return IS_NOT_CHANGED, None, f"RPC error: {resp.error}"
+
+    return IS_CHANGED, resp, None
+
+
+def preflight(conn: ovs.jsonrpc.Connection) -> tuple[dict[str, str] | None, Err | None]:
     request = ovs.jsonrpc.Message.create_request("transact", [
         "Open_vSwitch",
         {"op": "select", "table": "Open_vSwitch", "where": [], "columns": ["external_ids"] }
         ])
     err, resp = conn.transact_block(request)
     if err:
-        return None, NOT_EXISTS, os.strerror(err)
+        return None, os.strerror(err)
     if resp is None:
-        return None, NOT_EXISTS, "OVSDB server did not reply"
+        return None, "OVSDB server did not reply"
     if resp.type == ovs.jsonrpc.Message.T_ERROR or resp.error is not None:
-        return None, NOT_EXISTS, f"RPC error: {resp.error}"
-    ids = dict(resp.result[0]["rows"][0]["external_ids"][1])
-    if ids.get(key):
-        return ids.get(key), EXISTS, None
-    return None, NOT_EXISTS, None
-
-def clearKey(conn: ovs.jsonrpc.Connection, key: str, val: Any) -> tuple[Changed, str | None]:
-    request = ovs.jsonrpc.Message.create_request("transact", [
-        "Open_vSwitch",
-        {"op": "mutate", "table": "Open_vSwitch", "where": [], "mutations": [["external_ids", "delete", ["map", [[key, val]]] ]] }
-        ])
-    err, resp = conn.transact_block(request)
-    if err:
-        return IS_NOT_CHANGED, os.strerror(err)
-    if resp is None:
-        return IS_NOT_CHANGED, "OVSDB server did not reply"
-    if resp.type == ovs.jsonrpc.Message.T_ERROR or resp.error is not None:
-        return IS_NOT_CHANGED, f"RPC error: {resp.error}"
-
-    return IS_CHANGED, None
-
-def createKey(conn: ovs.jsonrpc.Connection, key: str, val: Any) -> tuple[Changed, str | None]:
-    request = ovs.jsonrpc.Message.create_request("transact", [
-        "Open_vSwitch",
-        {"op": "mutate", "table": "Open_vSwitch", "where": [], "mutations": [["external_ids", "insert", ["map", [[key, val]]] ]] }
-        ])
-    err, resp = conn.transact_block(request)
-    if err:
-        return IS_NOT_CHANGED, os.strerror(err)
-    if resp is None:
-        return IS_NOT_CHANGED, "OVSDB server did not reply"
-    if resp.type == ovs.jsonrpc.Message.T_ERROR or resp.error is not None:
-        return IS_NOT_CHANGED, f"RPC error: {resp.error}"
-
-    return IS_CHANGED, None
+        return None, f"RPC error: {resp.error}"
+    return dict(resp.result[0]["rows"][0]["external_ids"][1]), None
 
 def runModule(module: AnsibleModule):
     error, stream = ovs.stream.Stream.open_block(ovs.stream.Stream.open(module.params["remote"]))
     if error:
-        return True, False, os.strerror(error)
+        return False, None, os.strerror(error)
     rpc = ovs.jsonrpc.Connection(stream)
-    for i in module.params["ext_ids"]:
-        req = i.split("=")
-        res, exist, err = checkKey(rpc, req[0])
-        if module.check_mode:
-            continue
-        if err:
-            return True, False, err
-        if exist:
-            changed, err = clearKey(rpc, req[0], res)
-            if err or not changed:
-                return True, False, err
-        changed, err = createKey(rpc, req[0], req[1])
-        if err or not changed:
-            return True, False, err
-
-    return False, True, ''
+    newState = dict(i.split("=", 1) for i in module.params["ext_ids"])
+    return MakeTransaction(rpc, newState, module.check_mode)
 
 
 def main():
@@ -105,11 +88,10 @@ def main():
         supports_check_mode=True
     )
     try:
-        is_err, has_changed, result = runModule(module)
-        if not is_err:
-            module.exit_json(changed=has_changed, output=result)
-        else:
-            module.fail_json(msg=result)
+        chg, res, err = runModule(module)
+        if err:
+            module.fail_json(changed=chg, msg=err)
+        module.exit_json(changed=chg, msg=str(res))
     except Exception as e:
         module.fail_json(msg=str(e), exception=traceback.format_exc())
 
