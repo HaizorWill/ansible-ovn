6cda2ca01d8044dd8c4e128aa8def48755d9abcd
HaizorWill <Thelines6@gmail.com>
2026-06-10 14:09:44 +0300

    Add clearKey and createKey
    On the sidenode this needs to be atomic, but it works as of now, kinda

--- library/ovsdb-externalids.py
+++ library/ovsdb-externalids.py
@@ -2,36 +2,74 @@
 
 from __future__ import (absolute_import, division, print_function)
 import os
+from re import split
 import time
+from typing import Any
 __metaclass__ = type
 
 from ansible.module_utils.basic import AnsibleModule
+from typing import NewType
+
+Exists = NewType("Exists", bool)
+EXISTS: Exists = Exists(True)
+NOT_EXISTS: Exists = Exists(False)
+
+Changed = NewType("Changed", bool)
+IS_CHANGED: Changed = Changed(True)
+IS_NOT_CHANGED: Changed = Changed(False)
+
 import traceback
 import ovs.stream
 import ovs.dirs
 import ovs.poller
 import ovs.jsonrpc
 
-def checkKey(conn: ovs.jsonrpc.Connection, key: str):
+def checkKey(conn: ovs.jsonrpc.Connection, key: str) -> tuple[Any, Exists, str | None]:
     request = ovs.jsonrpc.Message.create_request("transact", [
         "Open_vSwitch",
         {"op": "select", "table": "Open_vSwitch", "where": [], "columns": ["external_ids"] }
         ])
     err, resp = conn.transact_block(request)
     if err:
-        return False, os.strerror(err)
+        return None, NOT_EXISTS, os.strerror(err)
     if resp is None:
-        return False, "OVSDB server did not reply"
+        return None, NOT_EXISTS, "OVSDB server did not reply"
     if resp.type == ovs.jsonrpc.Message.T_ERROR or resp.error is not None:
-        return False, f"RPC error: {resp.error}"
+        return None, NOT_EXISTS, f"RPC error: {resp.error}"
     ids = dict(resp.result[0]["rows"][0]["external_ids"][1])
     if ids.get(key):
-        print(ids)
-        return True, None
-    return False, None
+        return ids.get(key), EXISTS, None
+    return None, NOT_EXISTS, None
 
-def clearKey():
-   return 
+def clearKey(conn: ovs.jsonrpc.Connection, key: str, val: Any) -> tuple[Changed, str | None]:
+    request = ovs.jsonrpc.Message.create_request("transact", [
+        "Open_vSwitch",
+        {"op": "mutate", "table": "Open_vSwitch", "where": [], "mutations": [["external_ids", "delete", ["map", [[key, val]]] ]] }
+        ])
+    err, resp = conn.transact_block(request)
+    if err:
+        return IS_NOT_CHANGED, os.strerror(err)
+    if resp is None:
+        return IS_NOT_CHANGED, "OVSDB server did not reply"
+    if resp.type == ovs.jsonrpc.Message.T_ERROR or resp.error is not None:
+        return IS_NOT_CHANGED, f"RPC error: {resp.error}"
+
+    return IS_CHANGED, None
+
+def createKey(conn: ovs.jsonrpc.Connection, key: str, val: Any) -> tuple[Changed, str | None]:
+    request = ovs.jsonrpc.Message.create_request("transact", [
+        "Open_vSwitch",
+        {"op": "mutate", "table": "Open_vSwitch", "where": [], "mutations": [["external_ids", "insert", ["map", [[key, val]]] ]] }
+        ])
+    err, resp = conn.transact_block(request)
+    if err:
+        return IS_NOT_CHANGED, os.strerror(err)
+    if resp is None:
+        return IS_NOT_CHANGED, "OVSDB server did not reply"
+    if resp.type == ovs.jsonrpc.Message.T_ERROR or resp.error is not None:
+        return IS_NOT_CHANGED, f"RPC error: {resp.error}"
+
+    return IS_CHANGED, None
 
 def runModule(module: AnsibleModule):
     error, stream = ovs.stream.Stream.open_block(ovs.stream.Stream.open(module.params["remote"]))
@@ -40,13 +78,18 @@ def runModule(module: AnsibleModule):
     rpc = ovs.jsonrpc.Connection(stream)
     for i in module.params["ext_ids"]:
         req = i.split("=")
-        exist, err = checkKey(rpc, req[0])
+        res, exist, err = checkKey(rpc, req[0])
         if module.check_mode:
             continue
         if err:
             return True, False, err
         if exist:
-            clearKey()
+            changed, err = clearKey(rpc, req[0], res)
+            if err or not changed:
+                return True, False, err
+        changed, err = createKey(rpc, req[0], req[1])
+        if err or not changed:
+            return True, False, err
 
     return False, True, ''
 
